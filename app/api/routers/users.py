63944b1c8bde90a3ee795febccced08ae3b72a06
Vlad Novice <hakettip@mail.ru>
2025-04-05 20:07:14 +0500

    reg/login JWT

--- app/api/routers/users.py
+++ app/api/routers/users.py
@@ -10,7 +10,7 @@ from sqlalchemy import select
 from database.models import User
 from schemas.user import UserCreate, UserResponce, Token
 from database.base import get_db_session
-from core.security import verify_password, status, create_access_token, timedelta, ACCESS_TOKEN_EXPIRE_MINUTES
+from core.security import verify_password, status, create_access_token, timedelta, ACCESS_TOKEN_EXPIRE_MINUTES, get_password_hash
 
 # other 
 
@@ -23,7 +23,34 @@ user_router = APIRouter(prefix='/users', tags=['users'])
 
 
 @user_router.post("/registred", response_model=Token)
+async def register_user(
+    user_data: UserCreate,
+    db: AsyncSession = Depends(get_db_session),
+):
+    ex_user = await db.execute(select(User).where(User.name == user_data.name))
+    res_ex_user = ex_user.scalar_one_or_none()
 
+    if res_ex_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Юзернейм уже занят"
+        )
+    
+    new_user = User(
+        name=user_data.name,
+        password_hash=get_password_hash(user_data.password)
+    )
+
+    db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
+
+    access_token = create_access_token(
+        data={"sub": new_user.name},
+        expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
 
 @user_router.post("/login", response_model=Token)
 async def login_user(
