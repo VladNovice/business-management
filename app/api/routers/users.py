3f42fd9dc639e4429f7c2c086136f4d313aa3d3e
Vlad Novice <hakettip@mail.ru>
2025-04-05 19:44:20 +0500

    jwt update

--- app/api/routers/users.py
+++ app/api/routers/users.py
@@ -4,20 +4,30 @@ from fastapi import Depends
 
 # sqlalchemy imports
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 # project imports
 from database.models import User
 from schemas.user import UserCreate, UserResponce
 from database.base import get_db_session
 
+# other 
+from authx import AuthX, AuthXConfig
 
+
+
+config = AuthXConfig()
+config.JWT_SECRET_KEY = "SECRET_KEY"
+config.JWT_ACCESS_COOKIE_NAME = "my_acces_token"
+config.JWT_TOKEN_LOCATION = ["cookies"]
+
+security = AuthX(config=config)
 
 user_router = APIRouter(prefix='/users', tags=['users'])
 
-@user_router.post("/registred",response_model=UserResponce)
+@user_router.post("/login",response_model=UserResponce)
 async def register_user(
     user_data: UserCreate,
     db: AsyncSession = Depends(get_db_session),
 
 ):
-    existing_user = await db.execute
